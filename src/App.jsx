11ac01958c8f75925823ba7376eb4539d553fef8
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-12 15:09:12 +0300

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -55,23 +55,11 @@ export default function App() {
     });
   }
 
-  /**
-   * Challenge: complete and implement the deleteNote function
-   *
-   * Hints:
-   * 1. What array method can be used to return a new
-   *    array that has filtered out an item based
-   *    on a condition?
-   * 2. Notice the parameters being based to the function
-   *    and think about how both of those parameters
-   *    can be passed in during the onClick event handler
-   */
-
   function deleteNote(event, noteId) {
     event.stopPropagation();
     setNotes(
       prevNotes => prevNotes.filter(
-        note => !(note.id === noteId)));
+        note => note.id !== noteId));
   }
 
   function findCurrentNote() {
